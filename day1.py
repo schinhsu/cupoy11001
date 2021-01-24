19f7c82c1dbd2fde5e6c8510bdffdf09e8bdd1bd
schsu <s710409@hotmail.com.tw>
2021-01-24 17:46:10 +0800

    Day 1: amend

--- day1.py
+++ day1.py
@@ -3,8 +3,10 @@ import numpy
 # 題目:
 # 1.生成一個等差數列，首數為0，尾數為20，公差為1的數列。
 a = numpy.arange(0, 21)
-print(a)
+# print(a)
 # 2.呈上題，將以上數列取出偶數。
-print(a[::2])
+b = a[::2]
+# print(b)
 # 3.呈1題，將數列取出3的倍數。
-print(a[::3])
+c = a[::3]
+# print(c)
